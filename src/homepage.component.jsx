6c88473a4f1960005d52123144be71d118ee4a54
Guedelho <mateus.guedelho@gmail.com>
2020-02-10 20:55:43 -0600

    Add homepage component and styles

---
+++ src/homepage.component.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import './homepage.styles.scss'
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <div className="title">
+                        HAT
+                    </div>
+                    <span className="subtitle">
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <div className="title">
+                        HAT
+                    </div>
+                    <span className="subtitle">
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <div className="title">
+                        HAT
+                    </div>
+                    <span className="subtitle">
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <div className="title">
+                        HAT
+                    </div>
+                    <span className="subtitle">
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <div className="title">
+                        HAT
+                    </div>
+                    <span className="subtitle">
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage;
